93141909bab5323abb4d507fa1e13058a7e7eeaf
tazeenida <156009825+tazeenida@users.noreply.github.com>
2024-04-25 18:31:09 -0400

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,6 @@
-// Import the react JS packages 
 import axios from "axios";
 import { useState } from "react";
 
-// Define the Login function.
 export const Login = () => {
      const [username, setUsername] = useState('');
      const [password, setPassword] = useState('');
