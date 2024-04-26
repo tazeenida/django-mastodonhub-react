6468f0f55c281d09ed31a6423e579cc6887f5b41
Nida <tazen01@pfw.edu>
2024-04-25 20:03:20 -0400

    updated cors whitelist

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,5 +169,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://django-mastodonhub-react-1.onrender.com/'
 ]
