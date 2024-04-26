c1fda7b8966a0d55f0490153018a2b21af06d2fa
tazeenida <156009825+tazeenida@users.noreply.github.com>
2024-04-25 23:51:26 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,7 @@ function App() {
           </>
         ) : (
           <>
-            <Route path="/" element={<Navigate to="/Login" />} />
+            <Route path="/Login" element={<Navigate to="/Login" />} />
             <Route path="/signup" element={<Signup />} />
           </>
         )}
